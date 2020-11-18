8a837e86096174a141144b4bc1a296275a2db6f2
linzj <manjian2006@gmail.com>
2020-11-18 22:00:46 +0800

    Use range as the detect predictor.

--- injectscript.js
+++ injectscript.js
@@ -6,15 +6,26 @@
     let video_media_type = "webm";
     let audio_media_type = "webm";
     let video_playbackRateTimer = 0;
-    let video = null;
+    let started = false;
+    let last_video_stop = -1;
+    let last_audio_stop = -1;
 
-    function onVideoContent(buffer, rn) {
-        video_array_buffer_parts[rn] = buffer;
-        last_video_rn = rn;
+    function onVideoContent(buffer, range) {
+        let split = range.split('-');
+        let start = parseInt(split[0]);
+        if (start != last_video_stop + 1)
+            return;
+        last_video_stop = parseInt(split[1]);
+        video_array_buffer_parts[range] = buffer;
     }
 
-    function onAudioContent(buffer, rn) {
-        audio_array_buffer_parts[rn] = buffer;
+    function onAudioContent(buffer, range) {
+        let split = range.split('-');
+        let start = parseInt(split[0]);
+        if (start != last_audio_stop + 1)
+            return;
+        last_audio_stop = parseInt(split[1]);
+        audio_array_buffer_parts[range] = buffer;
     }
 
     function mayChangeMediaType(content_type, old_media_type) {
@@ -36,55 +47,58 @@
         if (method.toUpperCase() == "GET" && url.includes('videoplayback')) {
             this.should_pay_attention = true;
             let myurl = new URL(url);
-            let rn = myurl.searchParams.get('rn');
-            this.rn = rn;
-            console.log(`rn: ${rn}`);
-            console.log(`should pay attention for host: ${myurl.host}, hostname: ${myurl.hostname}, href: ${myurl.href}, pathname: ${myurl.pathname}`);
-            for (const [key, value] of myurl.searchParams) {
-                console.log(`key: ${key}, value: ${value}`);
-            }
-        } else {
-            this.should_pay_attention = false;
-        }
-        return old_open.apply(this, arguments);
-    }
-    XMLHttpRequest.prototype.addEventListener = function(type, listener, useCapture) {
-        if (this.should_pay_attention && type.toUpperCase() == "LOAD") {
+            let range = myurl.searchParams.get('range');
+            // console.log(`should pay attention for host: ${myurl.host}, hostname: ${myurl.hostname}, href: ${myurl.href}, pathname: ${myurl.pathname}`);
+            // for (const [key, value] of myurl.searchParams) {
+            //     console.log(`key: ${key}, value: ${value}`);
+            // }
             let thiz = this;
-            return old_addEventListener.apply(this, [type, function(e) {
+            this.addEventListener("load", (e) => {
                 if (thiz.readyState == 4 && thiz.status == 200) {
-                    console.log(`find a chunk ${thiz.getResponseHeader('content-type')}`);
+                    console.log(`find a chunk ${thiz.getResponseHeader('content-type')}, range: ${range}.`);
                     let content_type = thiz.getResponseHeader('content-type');
                     if (content_type.includes('video')) {
-                        if (thiz.rn == '1') {
-                            Reset();
+                        if (range.split('-')[0] == '0') {
                             Start();
                         }
                         try {
                             video_media_type = mayChangeMediaType(content_type, video_media_type);
                         } catch (e) {}
-                        onVideoContent(thiz.response, thiz.rn);
+                        onVideoContent(thiz.response, range);
                     } else if (content_type.includes('audio')) {
                         try {
                             audio_media_type = mayChangeMediaType(content_type, audio_media_type);
                         } catch (e) {}
-                        onAudioContent(thiz.response, thiz.rn);
+                        onAudioContent(thiz.response, range);
                     }
                 }
-                listener(e);
-            }, useCapture]);
+            });
+        } else {
+            this.should_pay_attention = false;
         }
-        return old_addEventListener.apply(this, arguments);
+        return old_open.apply(this, arguments);
     }
 
     function concatenate(arrays) {
         // Calculate byteSize from all arrays
         let size = 0;
-        let sorted_keys = [];
-        Object.keys(arrays).forEach((k) => {
-            sorted_keys.push(parseInt(k));
+        let sorted_keys = Object.keys(arrays);
+        sorted_keys.sort((a, b) => {
+            let a_range = a.split('-');
+            let b_range = b.split('-');
+            return parseInt(a_range[0]) - parseInt(b_range[0]);
         });
-        sorted_keys = sorted_keys.sort((a, b) => a - b);
+        // detect gaps
+        let last_range = undefined;
+        sorted_keys.forEach((k) => {
+            const current_split = k.split('-');
+            const current_range = [ parseInt(current_split[0]), parseInt(current_split[1])];
+            if (last_range) {
+                if (last_range[1] + 1 != current_range[0])
+                    console.log(`detected gaps at [${last_range[0]}, ${last_range[1]}) and [${current_range[0]}, ${current_range[1]})`);
+            }
+            last_range = current_range;
+        });
         for (let k of sorted_keys) {
             size += arrays[k].byteLength;
         }
@@ -126,19 +140,25 @@
 
     function Start() {
         console.log('Started');
+        started = true;
         let video = document.querySelectorAll('video')[0];
         video_playbackRateTimer = setInterval(() => {
+            video.playbackRate = 4;
             console.log(`video.playbackRate = ${video.playbackRate}`);
         }, 1000);
         video.addEventListener('ended', OnVideoEnded);
     }
 
     function Reset() {
+        started = false;
+        last_video_stop = -1;
+        last_audio_stop = -1;
         video_array_buffer_parts = {};
         audio_array_buffer_parts = {};
         video_media_type = "webm";
         audio_media_type = "webm";
         clearInterval(video_playbackRateTimer);
+        let video = document.querySelectorAll('video')[0];
         video?.removeEventListener('ended', OnVideoEnded);
     }
 })();
