0c54ae6f1f5a823e611b16f4da90f5de604d09c3
linzj <zuojian.lzj@alibaba-inc.com>
2020-11-18 18:10:44 +0800

    Buggy. needs range field.

--- injectscript.js
+++ injectscript.js
@@ -1,17 +1,20 @@
 (function() {
     let old_open = XMLHttpRequest.prototype.open;
     let old_addEventListener = XMLHttpRequest.prototype.addEventListener;
-    let video_array_buffer_parts = [];
-    let audio_array_buffer_parts = [];
+    let video_array_buffer_parts = {};
+    let audio_array_buffer_parts = {};
     let video_media_type = "webm";
     let audio_media_type = "webm";
+    let video_playbackRateTimer = 0;
+    let video = null;
 
-    function onVideoContent(buffer) {
-        video_array_buffer_parts.push(buffer);
+    function onVideoContent(buffer, rn) {
+        video_array_buffer_parts[rn] = buffer;
+        last_video_rn = rn;
     }
 
-    function onAudioContent(buffer) {
-        audio_array_buffer_parts.push(buffer);
+    function onAudioContent(buffer, rn) {
+        audio_array_buffer_parts[rn] = buffer;
     }
 
     function mayChangeMediaType(content_type, old_media_type) {
@@ -33,18 +36,13 @@
         if (method.toUpperCase() == "GET" && url.includes('videoplayback')) {
             this.should_pay_attention = true;
             let myurl = new URL(url);
-            let rbuf = myurl.searchParams.get('rbuf');
-            if (rbuf == '0') {
-                video_array_buffer_parts = [];
-                audio_array_buffer_parts = [];
-                video_media_type = "webm";
-                audio_media_type = "webm";
+            let rn = myurl.searchParams.get('rn');
+            this.rn = rn;
+            console.log(`rn: ${rn}`);
+            console.log(`should pay attention for host: ${myurl.host}, hostname: ${myurl.hostname}, href: ${myurl.href}, pathname: ${myurl.pathname}`);
+            for (const [key, value] of myurl.searchParams) {
+                console.log(`key: ${key}, value: ${value}`);
             }
-            console.log(`rbuf: ${myurl.searchParams.get('rbuf')}`);
-            // console.log(`should pay attention for host: ${myurl.host}, hostname: ${myurl.hostname}, href: ${myurl.href}, pathname: ${myurl.pathname}`);
-            // for (const [key, value] of myurl.searchParams) {
-            //     console.log(`key: ${key}, value: ${value}`);
-            // }
         } else {
             this.should_pay_attention = false;
         }
@@ -58,15 +56,19 @@
                     console.log(`find a chunk ${thiz.getResponseHeader('content-type')}`);
                     let content_type = thiz.getResponseHeader('content-type');
                     if (content_type.includes('video')) {
+                        if (thiz.rn == '1') {
+                            Reset();
+                            Start();
+                        }
                         try {
                             video_media_type = mayChangeMediaType(content_type, video_media_type);
                         } catch (e) {}
-                        onVideoContent(thiz.response);
+                        onVideoContent(thiz.response, thiz.rn);
                     } else if (content_type.includes('audio')) {
                         try {
                             audio_media_type = mayChangeMediaType(content_type, audio_media_type);
                         } catch (e) {}
-                        onAudioContent(thiz.response);
+                        onAudioContent(thiz.response, thiz.rn);
                     }
                 }
                 listener(e);
@@ -77,15 +79,24 @@
 
     function concatenate(arrays) {
         // Calculate byteSize from all arrays
-        let size = arrays.reduce((a, b) => a + b.byteLength, 0);
+        let size = 0;
+        let sorted_keys = [];
+        Object.keys(arrays).forEach((k) => {
+            sorted_keys.push(parseInt(k));
+        });
+        sorted_keys = sorted_keys.sort((a, b) => a - b);
+        for (let k of sorted_keys) {
+            size += arrays[k].byteLength;
+        }
         // Allcolate a new buffer
         let result = new Uint8Array(size);
 
         // Build the new array
         let offset = 0;
-        for (let arr of arrays) {
-            result.set(new Uint8Array(arr), offset)
-            offset += arr.byteLength;
+        for (let k of sorted_keys) {
+            let buf = arrays[k];
+            result.set(new Uint8Array(buf), offset)
+            offset += buf.byteLength;
         }
 
         return result;
@@ -105,17 +116,29 @@
         a.click();
         a.remove();
     }
-    window.addEventListener('load', () => {
+
+    function OnVideoEnded(e) {
+        console.log('OnVideoEnded');
+        SaveData('video', video_array_buffer_parts, video_media_type);
+        SaveData('audio', audio_array_buffer_parts, audio_media_type);
+        Reset();
+    }
+
+    function Start() {
+        console.log('Started');
         let video = document.querySelectorAll('video')[0];
-        let video_playbackRateTimer = setInterval(() => {
-            video.playbackRate = 16.0;
+        video_playbackRateTimer = setInterval(() => {
+            console.log(`video.playbackRate = ${video.playbackRate}`);
         }, 1000);
-        video.addEventListener('ended', (e) => {
-            SaveData('video', video_array_buffer_parts, video_media_type);
-            SaveData('audio', audio_array_buffer_parts, audio_media_type);
-            video_array_buffer_parts = []
-            audio_array_buffer_parts = []
-            clearInterval(video_playbackRateTimer);
-        });
-    });
+        video.addEventListener('ended', OnVideoEnded);
+    }
+
+    function Reset() {
+        video_array_buffer_parts = {};
+        audio_array_buffer_parts = {};
+        video_media_type = "webm";
+        audio_media_type = "webm";
+        clearInterval(video_playbackRateTimer);
+        video?.removeEventListener('ended', OnVideoEnded);
+    }
 })();
