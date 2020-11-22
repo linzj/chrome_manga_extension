417fe70ab017278f753891af6df0ba4689cf31f9
linzj <manjian2006@gmail.com>
2020-11-22 09:10:50 +0800

    Change ImageServer to python3

--- ImageServer.py
+++ ImageServer.py
@@ -1,30 +1,30 @@
-#!/usr/bin/env python2
+#!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-from BaseHTTPServer import HTTPServer, BaseHTTPRequestHandler
-from SocketServer import ThreadingMixIn
+from http.server import HTTPServer, BaseHTTPRequestHandler
+from socketserver import ThreadingMixIn
 import threading
-import cgi, StringIO, base64, os, sys, locale, re, io
+import cgi, base64, os, sys, locale, re, io
 import os.path
-from urllib import unquote
+from urllib.parse import unquote
 
 class Handler(BaseHTTPRequestHandler):
     def do_POST(self):
-        ctype, pdict = cgi.parse_header(self.headers.getheader('content-type'))
-        contentLength = int(self.headers.getheader('content-length'))
-        _range = self.headers.getheader('range')
+        ctype, pdict = cgi.parse_header(self.headers['content-type'])
+        contentLength = int(self.headers['content-length'])
+        _range = self.headers['range']
         self.saveInput(self.rfile.read(contentLength), _range)
         self.send_response(200)
         self.send_header('Access-Control-Allow-Origin', '*')
         self.end_headers()
-        message =  threading.currentThread().getName()
-        self.wfile.write(message)
-        self.wfile.write('\n')
+        message = threading.currentThread().getName()
+        self.wfile.write(message.encode('utf-8'))
+        self.wfile.write('\n'.encode('utf-8'))
         return
     def do_OPTIONS(self):
         self.send_response(200)
         self.send_header('Access-Control-Allow-Origin', '*')
-        self.send_header('Access-Control-Allow-Headers', self.headers.getheader('Access-Control-Request-Headers'))
+        self.send_header('Access-Control-Allow-Headers', self.headers['Access-Control-Request-Headers'])
         self.send_header('Access-Control-Allow-Methods', 'POST,OPTIONS')
         self.end_headers()
         return
@@ -33,16 +33,16 @@ class Handler(BaseHTTPRequestHandler):
         path = path[1:]
         path = unquote(path)
         if sys.platform == 'win32':
-            path = path.replace(':', "\xef\xbc\x9a")
-            path = path.replace('?', "_")
-            path = path.replace('<', "_")
-            path = path.replace('>', "_")
-            path = path.replace('"', "_")
-            path = path.replace('|', "_")
-            path = path.replace('\\', "_")
-            path = path.replace('\/', "_")
+            path = path.encode('utf-8')
+            path = path.replace(b':', b"\xef\xbc\x9a")
+            path = path.replace(b'?', b"_")
+            path = path.replace(b'<', b"_")
+            path = path.replace(b'>', b"_")
+            path = path.replace(b'"', b"_")
+            path = path.replace(b'|', b"_")
+            path = path.replace(b'\\', b"_")
+            path = path.replace(b'\/', b"_")
             path = path.decode('utf-8')
-            path = path.encode(sys.getfilesystemencoding())
         mode = 'r+b'
         if not os.path.exists(path):
             mode = 'wb'
@@ -62,5 +62,5 @@ class ThreadedHTTPServer(ThreadingMixIn, HTTPServer):
 
 if __name__ == '__main__':
     server = ThreadedHTTPServer(('localhost', 8787), Handler)
-    print 'Starting server, use <Ctrl-C> to stop'
+    print('Starting server, use <Ctrl-C> to stop')
     server.serve_forever()
